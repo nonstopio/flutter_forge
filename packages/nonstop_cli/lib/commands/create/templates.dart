72d97066cff120c0b1537de6cda63c26440f6042
Ajay Kumar <ajay.kumar@nonstopio.com>
2025-03-22 18:44:20 +0530

    feat(nonstop_cli): add Flutter plugin support for mono-repo with initial templates and hooks

--- packages/nonstop_cli/lib/commands/create/templates.dart
+++ packages/nonstop_cli/lib/commands/create/templates.dart
@@ -6,6 +6,7 @@ import 'package:nonstop_cli/utils/utils.dart';
 
 import 'flutter_app_for_mono_repo_bundle.dart';
 import 'flutter_package_for_mono_repo_bundle.dart';
+import 'flutter_plugin_for_mono_repo_bundle.dart';
 import 'flutter_project_with_mono_repo_bundle.dart';
 
 class FlutterProjectWithMonoRepoTemplate extends Template {
@@ -55,6 +56,23 @@ class FlutterAppForMonoRepoTemplate extends Template {
     logger.logSummary(
       outputDir: outputDir,
       message: 'Created a Flutter application for a Melos-managed mono-repo 🚀',
+    );
+  }
+}
+
+class FlutterPluginForMonoRepoTemplate extends Template {
+  FlutterPluginForMonoRepoTemplate()
+      : super(
+          name: 'flutterPluginForMonoRepoBundle',
+          bundle: flutterPluginForMonoRepoBundle,
+          help: 'A Flutter plugin for a Melos-managed mono-repo',
+        );
+
+  @override
+  Future<void> onGenerateComplete(Logger logger, Directory outputDir) async {
+    logger.logSummary(
+      outputDir: outputDir,
+      message: 'Created a Flutter plugin for a Melos-managed mono-repo 🚀',
     );
   }
 }
